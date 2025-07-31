bcf443c86155a539f6e33d8629975386efa1f913
KISHORE G <147577562+Kishore93442@users.noreply.github.com>
2025-07-31 13:32:59 +0530

    Delete CRM project directory

--- CRM project/Seller/seller.js
+++
@@ -1,6 +0,0 @@
-window.onload= function () {
-    const userlogin=JSON.parse(localStorage.getItem("loginuser"));
-    if(userlogin){
-        document.getElementById("wel").innerHTML="Welcome " + userlogin.Name
-    }
-}
